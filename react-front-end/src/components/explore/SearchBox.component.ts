005125d52de681ed49a792efcabd1ba99b869a97
bing <sabine@Macintosh-8.local>
2020-01-14 11:56:29 -0800

    styling for search box

--- react-front-end/src/components/explore/SearchBox.component.ts
+++ react-front-end/src/components/explore/SearchBox.component.ts
@@ -1,32 +1,45 @@
 import styled from "styled-components";
 
 export const Input = styled.input`
-  margin: 0px auto;
-  margin-top: 30px;
-  display: grid;
-  border-color: #D1D0CC
-  border-radius: 5px;
-  border-width: thin;
-  padding: 0px 10px;
+  border: 1px solid #D1D0CC;
+  border-radius: 2px;
   height: 40px;
-  width: 300px;
+  width: 98%;
   color: black;
+  text-align: center;
+  font-size: 16px;
 `;
 
 export const Suggestion = styled.p`
-  color: red;
+  color: #F55D3E;
+  // position: absolute;
+  width: 90%;
+  background-color: #FCFCFC;
+  // border: 1px solid #d4d4d4;
+  // padding: 12px 16px;
+  // z-index: 99;
 `;
 export const DatePick = styled.div`
-  margin: 5px auto;
-  display: inline-block;
-  // width: 100px;
+  width: 100%;
+  margin: 10px 0;
 `;
 
 export const Button = styled.button`
-  margin: 5px auto;
-  // width: 100px;
+  background-color: #76BED0;
+  color: #FCFCFC;
+  cursor: pointer;
+  border: none;
+  width: 100%;
+  padding: 15px;
+  margin: 25px 0 0 0;
+  font-size: 20px;
 `;
 
 export const Header = styled.h2`
-  top: 30px;
+  text-align: center;
+`;
+
+export const Wrapper = styled.div`
+  margin: 0 auto;
+  padding: 20px;
 `;
--- react-front-end/src/components/explore/SearchBox.tsx
+++ react-front-end/src/components/explore/SearchBox.tsx
@@ -1,10 +1,11 @@
-import React, {FC, useState, Fragment} from "react";
-import { Redirect, useHistory} from "react-router-dom";
+import React, { FC, useState, Fragment } from "react";
+import { Redirect, useHistory } from "react-router-dom";
 import axios from "axios";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import moment from "moment";
-import { Input, Suggestion, DatePick, Button, Header } from "./SearchBox.component"
+import { Wrapper, Input, Suggestion, DatePick, Button, Header } from "./SearchBox.component"
+import './SearchBox.css';
 
 
 interface SearchProps {
@@ -21,7 +22,7 @@ interface SearchObj {
 };
 
 export const SearchBar: FC<SearchProps> = ({ handleInputChange, handleSubmit }) => {
-  
+
   //user city input
   const [search, setSearch] = useState<SearchObj>({ query: '', results: [] });
 
@@ -29,7 +30,7 @@ export const SearchBar: FC<SearchProps> = ({ handleInputChange, handleSubmit }) 
   const [startDate, setStartDate] = useState<Date | null>(new Date());
   const [endDate, setEndDate] = useState<Date | null>(new Date());
   const [itinerariesId, setItinerariesId] = useState<number | null>();
-  
+
   handleInputChange = (e) => {
     const city = e.target.value
     axios.get(`/api/itineraries/`, {
@@ -37,23 +38,23 @@ export const SearchBar: FC<SearchProps> = ({ handleInputChange, handleSubmit }) 
         city: e.target.value
       }
     })
-    .then(res => {
-      let result: Array<any>;
-      let suggestion: Array<any>;
-      result = res.data.predictions
-      suggestion =[];
-      result.map(each => {
-        suggestion.push(each.description.split(',')[0])
+      .then(res => {
+        let result: Array<any>;
+        let suggestion: Array<any>;
+        result = res.data.predictions
+        suggestion = [];
+        result.map(each => {
+          suggestion.push(each.description.split(',')[0])
+        })
+        setSearch({ query: city, results: suggestion })
       })
-      setSearch({ query: city, results: suggestion })
-    }) 
   };
 
   handleSubmit = () => {
     axios.defaults.baseURL = 'http://localhost:8081';
     const city = search.query;
     const cityImg = 'https://vancouver.ca/images/cov/feature/about-vancouver-landing-size.jpg';
-    
+
     //convert to UTC timezone - UNIX
 
     let convertStart = startDate.toString().slice(4, 15);
@@ -81,7 +82,7 @@ export const SearchBar: FC<SearchProps> = ({ handleInputChange, handleSubmit }) 
           data: {
             city,
             cityImg,
-            tripStart, 
+            tripStart,
             tripEnd
           },
           withCredentials: true,
@@ -90,50 +91,55 @@ export const SearchBar: FC<SearchProps> = ({ handleInputChange, handleSubmit }) 
           }
         }),
       ])
-      .then((res) => {
-        setItinerariesId(res[0].data);
-      })
+        .then((res) => {
+          setItinerariesId(res[0].data);
+        })
     }
 
   };
 
   return (
     itinerariesId ? <Redirect to={`/explore/${itinerariesId}`} />
-    :
-    <Fragment>
-      <Header>Where do you travel to next?</Header>
-        <div className="SearchBar">
-          <form>
-            <Input
+      :
+      <Wrapper>
+        <Fragment>
+          <Header>Where do you travel to next?</Header>
+          <div className="SearchBar">
+            <form>
+              <Input
                 type="text"
                 id="search-input"
                 placeholder="Please enter your destination"
-                onChange ={handleInputChange}
-                value = {search.query}
-            />
-            <Suggestion>{search.results}</Suggestion>
-          </form>
-        </div>
-      <DatePick>
-        <div>
-          <h4>Start Date</h4>
-          <DatePicker 
-            selected={startDate} 
-            onChange={date => setStartDate(date)} 
-          />
-        </div>
-      </DatePick>
-      <DatePick>
-        <div>
-        <h4>End Date</h4>
-          <DatePicker
-            selected={endDate}
-            onChange={date => setEndDate(date)}
-          />
-        </div>  
-      </DatePick>
-      <Button onClick={handleSubmit}>Search</Button>
+                onChange={handleInputChange}
+                value={search.query}
+              />
 
-    </Fragment>
+              {search.results.map(result => (
+                <Suggestion>{result}</Suggestion>
+              ))}
+            </form>
+          </div>
+          <DatePick>
+            <div>
+              <h4>Start Date</h4>
+              <DatePicker
+                selected={startDate}
+                onChange={date => setStartDate(date)}
+              />
+            </div>
+          </DatePick>
+          <DatePick>
+            <div>
+              <h4>End Date</h4>
+              <DatePicker
+                selected={endDate}
+                onChange={date => setEndDate(date)}
+              />
+            </div>
+          </DatePick>
+          <Button onClick={handleSubmit}>Search</Button>
+
+        </Fragment>
+      </Wrapper>
   );
 };
