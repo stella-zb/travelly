e59099466a876a73a1a698877ba9e8cbdcc80845
Michelle Siregar <michellesiregar@gmail.com>
2020-01-14 14:34:22 -0800

    style attractionlist and invite

--- react-front-end/src/components/mytrips/Attraction.tsx
+++ react-front-end/src/components/mytrips/Attraction.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-type PropTypes = { id: number, name: string, img: string, editable: boolean, deleteAttraction: any, firstName:string|null, lastName:string|null }
+type PropTypes = {
+  id: number;
+  name: string;
+  img: string;
+  editable: boolean;
+  deleteAttraction: any;
+  firstName: string | null;
+  lastName: string | null;
+};
 
 const Name = styled.div`
   text-align: center;
@@ -17,39 +25,52 @@ const Name = styled.div`
 `;
 
 const DeleteButton = styled.button`
-  background: #F55D3E;
-  border: solid 1px #F55D3E;
-  color: #FCFCFC;
-  border-radius: 15px;
+  background: #f55d3e;
+  border: solid 1px #f55d3e;
+  color: #fcfcfc;
+  border-radius: 7px;
   font-size: 12px;
   font-weight: 600;
-`
+`;
 
 const Submitter = styled.div`
   background: rgba(0, 0, 0, 0.7);
   color: #fff;
   border: solid 1px #fff;
   border-radius: 50%
-  height: 21px;
-  width: 21px;
-  line-height: 21px;
+  height: 22px;
+  width: 22px;
+  line-height: 22px;
   margin-top: 4px;
   float: right;
   position: relative;
   left: 5px;
-  font-size: 10px;
-`
+  font-size: 11px;
+`;
 
 const Actions = styled.div`
   text-align: right;
   position: relative;
-  bottom: 15px;
-`
+  bottom: 18px;
+`;
 
-export const Attraction = ({id, name, img, editable, deleteAttraction, firstName, lastName}: PropTypes) => {
+export const Attraction = ({
+  id,
+  name,
+  img,
+  editable,
+  deleteAttraction,
+  firstName,
+  lastName
+}: PropTypes) => {
   const Container = styled.div`
     padding: 10px 20px 15px 20px;
-    background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.65), rgba(0, 0, 0, 0.65)), url(${img});
+    background-image: linear-gradient(
+        to bottom,
+        rgba(0, 0, 0, 0.65),
+        rgba(0, 0, 0, 0.65)
+      ),
+      url(${img});
     background-size: 100%;
     background-position: center;
     border-radius: 15px;
@@ -58,13 +79,23 @@ export const Attraction = ({id, name, img, editable, deleteAttraction, firstName
 
   return (
     <Container>
-      {firstName && <Submitter>{firstName[0]}{lastName[0]}</Submitter>}
-      <Name><h2>{name}</h2></Name>
-      
+      {firstName && (
+        <Submitter>
+          {firstName[0]}
+          {lastName[0]}
+        </Submitter>
+      )}
+      <Name>
+        <h2>{name}</h2>
+      </Name>
+
       <Actions>
-        {editable && <DeleteButton onClick={() => deleteAttraction(id)}>DELETE</DeleteButton>}
+        {editable && (
+          <DeleteButton onClick={() => deleteAttraction(id)}>
+            DELETE
+          </DeleteButton>
+        )}
       </Actions>
     </Container>
-  )
-}
-
+  );
+};
--- react-front-end/src/components/mytrips/AttractionList.tsx
+++ react-front-end/src/components/mytrips/AttractionList.tsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
-import { Attraction } from './Attraction';
-import { Button } from './Button';
-import { InviteIcon } from './InviteIcon';
-import addattr from '../../images/addpin.svg';
-import axios from 'axios';
-import { Redirect } from 'react-router-dom';
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Attraction } from "./Attraction";
+import { Button } from "./Button";
+import { InviteIcon } from "./InviteIcon";
+import addattr from "../../images/addpin.svg";
+import axios from "axios";
+import { Redirect } from "react-router-dom";
 
 const Attractions = styled.ul`
   padding-left: 0px;
@@ -22,14 +22,14 @@ const Title = styled.h1`
 `;
 
 const AddAttr = styled.img`
-  width: 35px;
+  width: 30px;
 `;
 
 const Actions = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
-`
+`;
 
 const Header = styled.header`
   display: flex;
@@ -38,37 +38,41 @@ const Header = styled.header`
   align-items: center;
   padding-right: 10%;
   padding-left: 10%;
+  border-bottom: 1px solid #a4ada6;
+  background: #e5f9ff;
+  height: 90px;
 `;
 
 type PropTypes = {
-  id: string,
-  attractions: Array<any>,
-  deleteAttraction: any,
-  setInvite: any,
-  generate: any
-}
+  id: string;
+  attractions: Array<any>;
+  deleteAttraction: any;
+  setInvite: any;
+  generate: any;
+};
 
-export const AttractionList = ({ id, attractions, deleteAttraction, setInvite, generate }: PropTypes) => {
-
+export const AttractionList = ({id, attractions, deleteAttraction, setInvite, generate}: PropTypes) => {
   const [edit, setEdit] = useState<boolean>(false);
 
   const updateItinerary = () => {
-    axios.get(`/api/itineraries/${id}`, {
-      params: {
-        user: localStorage.userID
-      }
-    })
-    .then(() => {
-      setEdit(true);
-    })
+    axios
+      .get(`/api/itineraries/${id}`, {
+        params: {
+          user: localStorage.userID
+        }
+      })
+      .then(() => {
+        setEdit(true);
+      });
   };
-
 
   return (
     <>
       {edit && <Redirect to={`/explore/${id}`} />}
       <Header>
-        <Title>{attractions.length === 0 ? "Itinerary" : attractions[0].city}</Title>
+        <Title>
+          {attractions.length === 0 ? "Itinerary" : attractions[0].city}
+        </Title>
         <Actions>
           <AddAttr src={addattr} onClick={updateItinerary} />
           <InviteIcon id={id} setInvite={setInvite} />
@@ -76,7 +80,7 @@ export const AttractionList = ({ id, attractions, deleteAttraction, setInvite, g
       </Header>
 
       <Attractions>
-        {attractions.map(attraction =>
+        {attractions.map(attraction => (
           <AttractionItem key={attraction.id}>
             <Attraction
               id={attraction.attraction_id}
@@ -88,10 +92,10 @@ export const AttractionList = ({ id, attractions, deleteAttraction, setInvite, g
               lastName={attraction.last_name}
             />
           </AttractionItem>
-        )}
+        ))}
       </Attractions>
 
       <Button text="Generate" click={generate} />
     </>
-  )
-}
+  );
+};
